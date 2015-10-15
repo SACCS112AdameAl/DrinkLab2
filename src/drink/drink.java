e2b489672292139fc18d15ed6835d012f9a379fa
Al Adame <Adame570@yahoo.com>
2015-10-14 19:47:43 -0700

    drinking age

---
+++ src/drink/drink.java
@@ -0,0 +1,26 @@
+package drink;
+
+import java.util.Scanner;
+
+public class drink {
+    public static void main(String[] args) {
+ String name;
+ int yearBorn = 0;
+ Scanner input = new Scanner(System.in);
+ 
+ System.out.println("Enter your name");
+ name = input.next();
+		 
+System.out.println("What is your birth year");
+ int age = (2015-yearBorn);
+ 
+ System.out.printf("Hello %s you are %d years old \n",name , age);
+ 
+ if (age>=21);{
+ 
+ System.out.println("You can drink" );
+    } 
+ else(){ System.out.println("You cannot drink"));
+    }
+}
+}
